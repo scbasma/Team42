f3514cbaf641a8b9aa888de0b24a0075022a9f2e
Jason Berry <bloodlustr@gmail.com>
2015-05-12 17:03:05 -0700

    Added backtracking

--- Ramsey/simple_taboo_search-7.c
+++ Ramsey/simple_taboo_search-7.c
@@ -19,7 +19,9 @@
 
 #define TABOOSIZE (500)
 #define BIGCOUNT (9999999)
-
+#ifdef __APPLE__
+#  define error printf
+#endif
 /***
  *** example of very simple search for R(7,7) counter examples
  ***
@@ -234,6 +236,76 @@ int CliqueCount(int *g,
     return(count);
 }
 
+void
+TrySolve(int *oldG,int oldGSize,int* biggest)
+{
+	int* newG = (int *)malloc((oldGSize+1)*(oldGSize+1)*sizeof(int));
+	CopyGraph(oldG,oldGSize,newG,oldGSize+1);
+	for(int i=0; i < (oldGSize+1); i++)
+        {
+                if(rand() % 2 == 0)
+		{
+			newG[i*(oldGSize+1) + oldGSize] = 0; // last column
+                	newG[oldGSize*(oldGSize+1) + i] = 0; // last row
+		}
+		else
+		{
+			newG[i*(oldGSize+1) + oldGSize] = 1; // last column
+                	newG[oldGSize*(oldGSize+1) + i] = 1; // last row
+		}
+        }
+	int i=0;
+	void *taboo_list = FIFOInitEdge(TABOOSIZE);
+	while(1)
+	{
+		int count = CliqueCount(newG,oldGSize+1);
+        	if(count == 0)
+        	{
+			if(oldGSize+1>*biggest)
+			{
+				*biggest=oldGSize+1;
+				printf("Eureka!  Counter-example found!\n");
+				PrintGraph(newG,oldGSize+1);
+				if(oldGSize+1 > 60)
+					SendGraph(newG,oldGSize+1);
+                	}
+			TrySolve(newG,oldGSize+1,biggest);
+		}
+		int last_count = count+1;
+		int best_count = last_count+1;
+		int best_i;
+		for(i=0; i < (oldGSize+1); i++)
+		{
+			newG[i*(oldGSize+1)+oldGSize] = 1 - newG[i*(oldGSize+1)+oldGSize];
+                        count = CliqueCount(newG,oldGSize+1);
+			if(count==0){
+				FIFOInsertEdge(taboo_list,i,oldGSize);
+				if(oldGSize+1>*biggest)
+                        	{
+                                	*biggest=oldGSize+1;
+                                	printf("Eureka!  Counter-example found!\n");
+                                	PrintGraph(newG,oldGSize+1);
+					if(oldGSize+1 > 60)
+						SendGraph(newG,oldGSize+1);
+                        	}
+				TrySolve(newG,oldGSize+1,biggest);
+				continue;
+			}
+                        if((count < best_count) && !FIFOFindEdge(taboo_list,i,oldGSize))
+                        {
+                        	best_count = count;
+                        	best_i = i;
+                        }
+                        newG[i*(oldGSize+1)+oldGSize] = 1 - newG[i*(oldGSize+1)+oldGSize];
+		}
+		if(best_count > last_count) {
+			return;
+		}
+		newG[best_i*(oldGSize+1)+oldGSize] = 1 - newG[best_i*(oldGSize+1)+oldGSize];
+		FIFOInsertEdge(taboo_list,best_i,oldGSize);
+	}
+	free(newG);
+}
 
 int
 main(int argc,char *argv[])
@@ -249,173 +321,16 @@ main(int argc,char *argv[])
 	int best_j;
 	void *taboo_list;
         srand(time(NULL));
-	/*
-	 * start with graph of size 8
-	 */
-	gsize = 8;
+	gsize = 1;
 	g = (int *)malloc(gsize*gsize*sizeof(int));
 	if(g == NULL) {
 		exit(1);
 	}
-
-	/*
-	 * make a fifo to use as the taboo list
-	 */
-	taboo_list = FIFOInitEdge(TABOOSIZE);
-	if(taboo_list == NULL) {
-		exit(1);
-	}
-
-	/*
-	 * start out with all zeros
-	 */
 	memset(g,0,gsize*gsize*sizeof(int));
-
-	/*
-	 * while we do not have a publishable result
-	 */
-	while(gsize < 206)
-	{
-		/*
-		 * find out how we are doing
-		 */
-		count = CliqueCount(g,gsize);
-
-		/*
-		 * if we have a counter example
-		 */
-		if(count == 0)
-		{
-			printf("Eureka!  Counter-example found!\n");
-			PrintGraph(g,gsize);
-			if(gsize > 60)
-				SendGraph(g, gsize);
-			/*
-			 * make a new graph one size bigger
-			 */
-			new_g = (int *)malloc((gsize+1)*(gsize+1)*sizeof(int));
-			if(new_g == NULL)
-				exit(1);
-			/*
-			 * copy the old graph into the new graph leaving the
-			 * last row and last column alone
-			 */
-			CopyGraph(g,gsize,new_g,gsize+1);
-
-			/*
-			 * zero out the last column and last row
-			 */
-			for(i=0; i < (gsize+1); i++)
-			{
-				if(rand() % 2 == 0){ 
-					new_g[i*(gsize+1) + gsize] = 0; // last column
-					new_g[gsize*(gsize+1) + i] = 0; // last row
-				}
-				else{
-					new_g[i*(gsize+1) + gsize] = 1; // last column
-					new_g[gsize*(gsize+1) + i] = 1; // last row
-				}
-			}
-
-			/*
-			 * throw away the old graph and make new one the
-			 * graph
-			 */
-			free(g);
-			g = new_g;
-			gsize = gsize+1;
-
-			/*
-			 * reset the taboo list for the new graph
-			 */
-			taboo_list = FIFOResetEdge(taboo_list);
-
-			/*
-			 * keep going
-			 */
-			continue;
-		}
-
-		/*
-		 * otherwise, we need to consider flipping an edge
-		 *
-		 * let's speculative flip each edge, record the new count,
-		 * and unflip the edge.  We'll then remember the best flip and
-		 * keep it next time around
-		 *
-		 * only need to work with upper triangle of matrix =>
-		 * notice the indices
-		 */
-		best_count = BIGCOUNT;
-		for(i=0; i < gsize; i++)
-		{
-			for(j=i+1; j < gsize; j++)
-			{
-				/*
-				 * flip it
-				 */
-				g[i*gsize+j] = 1 - g[i*gsize+j];
-				count = CliqueCount(g,gsize);
-
-				/*
-				 * is it better and the i,j,count not taboo?
-				 */
-#ifdef EDGEONLY
-				if((count < best_count) && 
-					!FIFOFindEdge(taboo_list,i,j))
-#else
-				if((count < best_count) && 
-					!FIFOFindEdgeCount(taboo_list,i,j,count))
-#endif
-				{
-					best_count = count;
-					best_i = i;
-					best_j = j;
-				}
-
-				/*
-				 * flip it back
-				 */
-				g[i*gsize+j] = 1 - g[i*gsize+j];
-			}
-		}
-
-		if(best_count == BIGCOUNT) {
-			printf("no best edge found, terminating\n");
-			exit(1);
-		}
-		
-		/*
-		 * keep the best flip we saw
-		 */
-		g[best_i*gsize+best_j] = 1 - g[best_i*gsize+best_j];
-
-		/*
-		 * taboo this graph configuration so that we don't visit
-		 * it again
-		 */
-		count = CliqueCount(g,gsize);
-#ifdef EDGEONLY
-		FIFOInsertEdge(taboo_list,best_i,best_j);
-#else
-		FIFOInsertEdgeCount(taboo_list,best_i,best_j,count);
-#endif
-
-		printf("ce size: %d, best_count: %d, best edge: (%d,%d), new color: %d\n",
-			gsize,
-			best_count,
-			best_i,
-			best_j,
-			g[best_i*gsize+best_j]);
-
-		/*
-		 * rinse and repeat
-		 */
-	}
-
-	FIFODeleteGraph(taboo_list);
-
-
+	int* biggest=(int *)malloc(sizeof(int));
+        *biggest = 0;
+	TrySolve(g,1,biggest);
+        g[0]=1;
+	TrySolve(g,1,biggest);
 	return(0);
-
 }
